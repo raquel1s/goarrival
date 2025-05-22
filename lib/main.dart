66b4b2ca07157d32e31f756f2dc6dcb4f2a77fc0
Raquel <silva.raquel2903@gmail.com>
2025-05-22 20:25:12 -0300

    testando

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:goarrival/screens/home_screen.dart';
+
+void main() {
+  runApp(const goArrival());
+}
+
+class goArrival extends StatelessWidget {
+  const goArrival({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'GOARRIVAL',
+      theme: ThemeData(
+        primaryColor: const Color(0xFF12455C),
+        scaffoldBackgroundColor: const Color(0xFFF8F9F8),
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Color(0xFF12455C),
+          foregroundColor: Colors.white,
+          elevation: 0,
+        ),
+        colorScheme: ColorScheme.fromSwatch().copyWith(
+          primary: const Color(0xFF12455C),
+          secondary: const Color(0xFFF8F9F8),
+        ),
+        textTheme: const TextTheme(
+          bodyMedium: TextStyle(color: Colors.black87),
+          titleLarge: TextStyle(
+            color: Color(0xFF12455C),
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
