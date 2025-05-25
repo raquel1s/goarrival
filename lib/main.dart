3dfef28680d0188330e4618ec28dbb0f1b126e4b
Raquel <silva.raquel2903@gmail.com>
2025-05-25 11:34:36 -0300

    tirando o firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:goarrival/screens/cadastrar_viagem.dart';
 import 'package:goarrival/screens/login.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-
+void main() {
   runApp(const GoArrival());
 }
 
